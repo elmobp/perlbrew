97c38b1a404fb489d0eff02126a7b0b248b6885d
Michael Baker <michael.baker@bulletproof.net>
2020-07-08 12:44:54 +0800

    Begin support for ubuntu

--- manifests/params.pp
+++ manifests/params.pp
@@ -10,15 +10,6 @@
 #   Specify the root of your perlbew installation. Defaults to '/opt/perl5'.
 #
 class perlbrew::params {
-
-  case $::osfamily {
-    'RedHat': {
       $perlbrew_root      = '/opt/perl5'
       $perlbrew_init_file = '/etc/profile.d/perlbrew.sh'
-    }
-    default: {
-      fail("${::operatingsystem} not supported")
-    }
-  }
-
 }
