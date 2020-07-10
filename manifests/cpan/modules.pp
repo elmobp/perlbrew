4c049b944f7fd51ff38c3332825f663293c83c5f
Michael Baker <michael.baker@bulletproof.net>
2020-07-10 09:25:50 +0800

    install deps then module

--- manifests/cpan/modules.pp
+++ manifests/cpan/modules.pp
@@ -30,10 +30,10 @@ class perlbrew::cpan::modules (
   $merged_options = concat($default_options, $options)
   $install_opts = join($merged_options, ' ')
   $cpan_modules.each |$cpan_module| {
-    $cpan_command = "${perlbrew::perlbrew_root}/perls/perl-${perlbrew::perl::version}/bin/cpanm ${install_opts} ${cpan_module}"
+    $cpan_deps_command = "${perlbrew::perlbrew_root}/perls/perl-${perlbrew::perl::version}/bin/cpanm ${install_opts} ${cpan_module}"
     $file_path = regsubst($cpan_module, '::', '/', 'G')
-    exec {"install_perl_module_${cpan_module}":
-      command     => $cpan_command,
+    exec {"install_perl_module_deps_${cpan_module}":
+      command     => $cpan_deps_command,
       timeout     => 0,
       creates     => "${perlbrew::perlbrew_root}/perls/perl-${perlbrew::perl::version}/lib/${perlbrew::perl::version}/${file_path}",
       environment => [
@@ -44,13 +44,35 @@ class perlbrew::cpan::modules (
         "PERLBREW_MANPATH=${perlbrew::perlbrew_root}/perls/perl-${perlbrew::perl::version}/man",
         'HOME=/opt',
       ],
-      path    => [
-      "${perlbrew::perlbrew_root}/bin",
-      '/usr/bin',
-      '/usr/sbin',
-      '/bin',
-      '/sbin'
+      path        => [
+        "${perlbrew::perlbrew_root}/bin",
+        '/usr/bin',
+        '/usr/sbin',
+        '/bin',
+        '/sbin'
       ],
+    }
+    $cpan_command = "${perlbrew::perlbrew_root}/perls/perl-${perlbrew::perl::version}/bin/cpanm ${cpan_module}"
+    exec {"install_perl_module_${cpan_module}":
+      command     => $cpan_command
+      timeout     => 0,
+      creates     => "${perlbrew::perlbrew_root}/perls/perl-${perlbrew::perl::version}/lib/${perlbrew::perl::version}/${file_path}",
+      environment => [
+        "PERLBREW_ROOT=${perlbrew::perlbrew_root}",
+        'PERLBREW_HOME=/tmp/.perlbrew',
+        "PERLBREW_PERL=perl-${perlbrew::perl::version}",
+        "PERLBREW_PATH=${perlbrew::perlbrew_root}/bin:${perlbrew::perlbrew_root}/perls/perl-${perlbrew::perl::version}/bin",
+        "PERLBREW_MANPATH=${perlbrew::perlbrew_root}/perls/perl-${perlbrew::perl::version}/man",
+        'HOME=/opt',
+      ],
+      path        => [
+        "${perlbrew::perlbrew_root}/bin",
+        '/usr/bin',
+        '/usr/sbin',
+        '/bin',
+        '/sbin'
+      ],
+      require     => Exec["install_perl_module_deps_${cpan_module}"]
     }
   }
 
