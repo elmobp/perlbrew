114e6044d42464f78178908af8587a4cb9a227c2
Michael Baker <michael.baker@bulletproof.net>
2020-07-16 11:48:10 +0800

    Cleaner

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,6 +3,7 @@
 # This class installs Perlbrew and is meant to be called from perlbrew
 #
 class perlbrew::install {
+  include archive
 
   if !defined(Package['curl']) {
     package {'curl':
@@ -16,10 +17,16 @@ class perlbrew::install {
     group  => 'root',
     mode   => '0755',
   }
-
-  exec {'install_perlbrew':
-    environment => [ 'PERLBREW_ROOT=/opt/perl5', 'PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
-    command     => '/usr/bin/curl -L https://gist.githubusercontent.com/elmobp/1fea91fd2afd044c59b0e105dbfc1fea/raw/ee720518233b5e82cf037130f7933d9737dd6b95/installer.sh | /bin/bash',
+  ~> archive{'/tmp/perlbrew-install':
+    source => 'https://raw.githubusercontent.com/gugod/App-perlbrew/master/perlbrew-install'
+  }
+  ~> file{'/tmp/perlbrew-install':
+    ensure => file,
+    mode   => '0755'
+  }
+  ~> exec {'install_perlbrew':
+    environment => [ "PERLBREW_ROOT=${perlbrew::perlbrew_root}", 'PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin', 'HOME=/tmp'],
+    command     => '/tmp/perlbrew-install', 
     creates     => "${perlbrew::perlbrew_root}/bin/perlbrew",
     require     => Package['curl'],
   }
